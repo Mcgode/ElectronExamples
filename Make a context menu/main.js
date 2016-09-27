25b7467917ea424b2ed493c73a355c9f46dea84a
Mcgode <max@godefroy.net>
2016-09-27 18:46:51 +0200

    Code completed

--- Make a context menu/main.js
+++ Make a context menu/main.js
@@ -6,7 +6,65 @@
 const Electron = require('electron');
 
 // Call Electron sub-libraries
-const { BrowserWindow, Menu, MenuItem } = Electron;
+const { app, BrowserWindow, ipcMain, Menu, MenuItem, dialog } = Electron;
 
 
+var main_window;
+
+const home_url = `file://${__dirname}/home.html`;
+
+
+app.on('ready', () => {
+    createMainWindow()
+});
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+
+app.on('activate', () => {
+    if (main_window === null) {
+        createMainWindow()
+    }
+});
+
+
+function createMainWindow() {
+
+    main_window = new BrowserWindow({ width: 500, height: 500, show: false });
+
+    main_window.once('ready-to-show', () => { main_window.show() });
+
+    main_window.loadURL(home_url);
+
+    main_window.once('closed', () => { main_window = null })
+
+}
+
+var context_menu;
+
+ipcMain.on('requested-context-menu', () => {
+
+    // Context menu can be made from a template, or dynamically, like here
+    context_menu = new Menu();
+    context_menu.append(new MenuItem({
+        label: 'You can click here...',
+        click() {
+            // Fancy alert
+            dialog.showMessageBox(main_window, {
+                buttons: ['Ok'],
+                title: 'Message',
+                message: '... and you clicked. Good job!'
+            })
+        }
+    }));
+
+    // Makes the context menu pop up on mouse location
+    context_menu.popup(main_window)
+
+});
 
